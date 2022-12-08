99fcc900e889a96c7e2252fbc76ca74affb38061
selivon <olimpfruit@ya.ru>
2022-12-08 16:56:59 +0300

    homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,72 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+/*Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);*/
+
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+/*Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int num)
+{ 
+int count = Convert.ToString(num).Length;
+int advance = 0;
+int result = 0;
+for (int i = 0; i < count; i++)
+    {
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+return result;
+}
+int sumNumber = SumNumber(num);
+Console.WriteLine("Сумма цифр в числе равна: " + sumNumber);*/
+
+
+
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+/*int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int [] newArray = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return newArray;
+}
+void ShowArray (int[] array)
+{
+    Console.Write("Получившийся массив ->");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(a,min,max);
+ShowArray(myArray);*/
